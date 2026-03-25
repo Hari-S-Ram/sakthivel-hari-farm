1bd0b96466ee1511bd671e86069afc0004804532
Harisivaram S <161581899+Hari-S-Ram@users.noreply.github.com>
2026-03-25 19:27:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ function sendToWhatsApp(e){
     let variety = document.getElementById("variety").value;
     let phone = document.getElementById("phone").value;
     let qty = document.getElementById("qty").value;
-    let msg = document.getElementById("msg").value;
+    let location = document.getElementById("location").value;
 
     let text =
         "Name: " + name +
         "\nVariety: " + variety +
         "\nPhone: " + phone +
         "\nQuantity: " + qty +
-        "\nMessage: " + msg;
+        "\nLocation: " + location;
 
     window.open("https://wa.me/919360421569?text=" + encodeURIComponent(text));
 }
